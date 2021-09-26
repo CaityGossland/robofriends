5ae4813d265d085ecf85a79b4973766a9deb92e7
Caity Gossland <caitygossland@gmail.com>
2021-09-26 13:22:37 -0700

    Hardcodes more robots

--- src/robots.js
+++ src/robots.js
@@ -4,17 +4,40 @@ export const robots = [
         name: 'Beverly Barr',
         username: 'Bevba',
         email: 'bbarr@gmail.com'
-    },
-    {
+    },{
         id: 2,
         name: 'Jenna Joy',
         username: 'Jenjoy',
         email: 'jjoy@gmail.com'
-    },
-    {
+    },{
         id: 3,
         name: 'Randy Roll',
         username: 'Ranroll',
         email: 'rroll@gmail.com'
+    },{
+        id: 4,
+        name: 'Jessica King',
+        username: 'jking',
+        email: 'jessicak@gmail.com'
+    },{
+        id: 5,
+        name: 'Harley Camp',
+        username: 'Harley',
+        email: 'har.camp@gmail.com'
+    },{
+        id: 6,
+        name: 'Jordan Messer',
+        username: 'JMess',
+        email: 'jordanmesser3@gmail.com'
+    },{
+        id: 7,
+        name: 'Ingrid Alibi',
+        username: 'Ingali',
+        email: 'ingali@gmail.com'
+    },{
+        id: 8,
+        name: 'Caity Gosshawk',
+        username: 'Gosshawk ',
+        email: 'cgosshawk@gmail.com'
     }
 ]
