544bb88bcaaf9353ba12b13b5c1978d3c33a7f38
Caity Gossland <caitygossland@gmail.com>
2021-09-26 10:40:25 -0700

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Beverly Barr',
+        username: 'Bevba',
+        email: 'bbarr@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jenna Joy',
+        username: 'Jenjoy',
+        email: 'jjoy@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Randy Roll',
+        username: 'Ranroll',
+        email: 'rroll@gmail.com'
+    }
+]
